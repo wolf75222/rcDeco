4f7d09b2d611131865238e010fe6990de6ef7bef
desp0042 <romain.despoullains@etudiant.univ-reims.fr>
2022-11-12 14:49:11 +0100

    v1.2 Ajout Javascript et reglage de beug

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+    var reveals = document.querySelectorAll('.reveal');  
+
+    for( var i = 0; i < revealed.length; i++ ) {
+        var windowheight = window.innnerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint
+
+    }
+}
