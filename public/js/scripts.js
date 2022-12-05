58165104a5ba360fd847feaf5a2beba2c112d91c
Sir. wolf <46008114+wolf75222@users.noreply.github.com>
2022-12-05 20:41:17 +0100

    quelque modification todo: clean le code

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -19,21 +19,3 @@ function reveal() {
     }
 }
 
-// write a send mail function
-function sendMail(contactForm) {
-    emailjs.send("gmail", "template_1", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "project_request": contactForm.projectsummary.value
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        }
-    );
-    return false;  // To block from loading a new page
-}
-
